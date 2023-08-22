4b835119534f1271e36cf35c2ba3c419300bd957
Salari <joldfag@gmail.com>
2023-08-22 21:38:05 +1000

    T1N4

---
+++ T1N4.py
@@ -0,0 +1,14 @@
+emp_list_v1 = []
+emp_list_v2 = []
+def palyndrome_check():
+    string = input('Введите строку для проверки на палиндром: ')
+    for i in string:
+        emp_list_v1.append(i)
+        emp_list_v2.append(i)
+    emp_list_v1.reverse()
+    if emp_list_v1 == emp_list_v2:
+        return True
+    elif emp_list_v1 != emp_list_v2:
+        return False
+palyndrome = palyndrome_check()
+print(palyndrome)
